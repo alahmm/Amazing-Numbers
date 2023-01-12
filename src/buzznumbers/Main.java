65b86a44c389ec374ad19252390dd99e1c447c9b
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-01-12 09:37:43 +0100

    duck numbers

--- src/buzznumbers/Main.java
+++ src/buzznumbers/Main.java
@@ -7,43 +7,47 @@ public class Main {
         if (Integer.parseInt(number) < 1) {
             System.out.println("This number is not natural!");
             return;
-        }
-        if (Integer.parseInt(number) % 2 == 0) {
-            System.out.println("This number is Even.");
         } else {
-            System.out.println("This number is Odd.");
+            System.out.printf("Properties of %s%n", number);
+            if (Integer.parseInt(number) % 2 == 0) {
+                String str = """
+                                even: true
+                                 odd: false
+                        """;
+                System.out.print(str);
+            } else {
+                String str = """
+                                even: false
+                                 odd: true
+                        """;
+                System.out.print(str);
+            }
+            char character1 = number.charAt(number.length() - 1);
+            int character = character1 - 48;
+            if (Integer.parseInt(number) % 7 == 0 || character == 7) {
+
+                System.out.println("        buzz: true");
+            } else {
+                System.out.println("        buzz: false");
+            }
+/*            char character2 = number.charAt(0);
+            int characterNew = character2 - 48;*/
+            int counter =0;
+            for (int i = 1; i < number.length(); i++) {
+                char character3 = number.charAt(i);
+                int character4= character3 - 48;
+                if ((character4 == 0))  {
+                    counter ++;
+                }
+            }
+            if (counter != 0) {
+                System.out.println("        duck: true");
+            } else {
+                System.out.println("        duck: false");
+
+            }
         }
-        char character1 = number.charAt(number.length() - 1);
-        int character = character1 - 48;
-        if (Integer.parseInt(number) % 7 == 0 && character != 7 ) {
 
-            String str = """
-                    It is a Buzz number.
-                    Explanation:""";
-            System.out.println(str);
-            System.out.printf("%n%s is divisible by 7.", number);
-        } else if (character == 7 && Integer.parseInt(number) % 7 != 0) {
-            String str = """
-                    It is a Buzz number.
-                    Explanation:""";
-            System.out.println(str);
-            System.out.printf("%s ends with 7..", number);
-
-        } else if (Integer.parseInt(number) % 7 == 0 && character == 7 ) {
-            String str = """
-                    It is a Buzz number.
-                    Explanation:""";
-            System.out.println(str);
-            System.out.printf("%s is divisible by 7 and ends with 7.", number);
-
-        } else {
-            String str = """
-                    It is not a Buzz number.
-                    Explanation:""";
-            System.out.println(str);
-            System.out.printf("%s  is neither divisible by 7 nor does it end with 7.", number);
-
-        }
     }
 
     public static void main(String[] args) {
